2494f2049b3e842e0799143b6a3a4db8f860ab7c
przemekkamieniorz <przemekkamieniorz@PRZEMEKKAMIBB8D>
2021-04-13 14:51:16 +0200

    Przemodelowanie bazy oraz dodanie tabeli z danymi użytkownika

---
+++ WeatherAPI/Models/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WeatherAPI.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Token { get; set; }
+
+        [ForeignKey("WeatherDataModels")]
+        public int weatherID { get; set; }
+
+        public virtual ICollection<WeatherDataModel> WeatherDataModels { get; set; }
+    }
+}
---
+++ WeatherAPI/Models/WeatherDataModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using WeatherAPI.Models;
+
+namespace WeatherAPI.Models
+{
+    public class WeatherDataModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public string Description { get; set; }
+        public string Temp { get; set; }
+        public string TempMax { get; set; }
+        public string TempMin { get; set; }
+        public string imgWeatherIconUrl { get; set; }
+    }
+}
